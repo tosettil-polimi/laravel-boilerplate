59e6a8ed430c312ba7466c3f0fae255dbec95179
tosettil-polimi <info@tosettil.me>
2021-07-19 11:31:22 +0200

    Fix npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/style.scss', 'public/css/mtconsulting.css')
+    .options({
+        processCssUrls: false
+    });
