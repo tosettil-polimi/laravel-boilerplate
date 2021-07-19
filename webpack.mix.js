c5edcc98221bd50291d0b8ed123f3c1c4dd77fff
tosettil-polimi <info@tosettil.me>
2021-07-19 11:42:32 +0200

    Source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .sourceMaps(false, 'source-map')
     .sass('resources/sass/style.scss', 'public/css/mtconsulting.css')
     .options({
         processCssUrls: false
